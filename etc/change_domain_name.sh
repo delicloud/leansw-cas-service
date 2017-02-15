66359ee6d1f1259f7391a3243825cbdce12667aa
qmxie <qmxie@thoughtworks.com>
2017-02-15 18:14:49 +0800

    [#0] fix change domain name script

--- etc/change_domain_name.sh
+++ etc/change_domain_name.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-OLD_LDAP_URL="${OLD_LDAP_URL:=ldap://deliflow-ldap-server:389}"
-LDAP_URL="${LDAP_URL:=ldap://deliflow-ldap-server:389}"
+OLD_LDAP_URL="${OLD_LDAP_URL:=deliflow-ldap-server:389}"
+LDAP_URL="${LDAP_URL:=deliflow-ldap-server:389}"
 OLD_CAS_DOMAIN_NAME="${OLD_CAS_DOMAIN_NAME:=cas.dev.twleansw.com}"
 CAS_DOMAIN_NAME="${CAS_DOMAIN_NAME:=cas.dev.twleansw.com}"
 sed -i -- "s/${OLD_CAS_DOMAIN_NAME}/${CAS_DOMAIN_NAME}/g" /etc/cas/cas.properties
-sed -i -- "s/${OLD_LDAP_URL}/${TEMP_LDAP_URL}/g" /etc/cas/cas.properties
+sed -i -- "s/${OLD_LDAP_URL}/${LDAP_URL}/g" /etc/cas/cas.properties
